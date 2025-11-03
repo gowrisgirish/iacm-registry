e5803900945f161b67f1b9f9c18d30be1415898b
gowrisgirish <156236897+gowrisgirish@users.noreply.github.com>
2025-11-02 19:23:08 -0500

    Add S3 bucket with versioning and encryption

---
+++ terraform-s3-bucket/main.tf
@@ -0,0 +1,26 @@
+# terraform-s3-bucket/main.tf
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  tags   = var.tags
+}
+
+# Enable versioning if requested
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = var.enable_versioning ? "Enabled" : "Suspended"
+  }
+}
+
+# Enable default encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
